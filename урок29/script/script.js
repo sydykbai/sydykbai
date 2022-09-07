7a975d7dddea286f9670954055108c2ad577687d
sydykbai <102861779+sydykbai@users.noreply.github.com>
2022-09-07 20:59:33 +0600

    Add files via upload

---
+++ урок29/script/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+ let hiddenRow = $(".expandable");
+ $(".expand-btn").click(function () {
+    hiddenRow.toggleClass("hidden");
+    if (hiddenRow.is(":visible")) {
+        $(".expand-btn").html("Уменьшить");
+    }
+    else {
+        $(".expand-btn").html("Расширить");
+    }
+ });
+ $(".play-button").click(function () { 
+    $(".modal-canvas").toggleClass("hidden");
+ });
+ $(".modal-close").click( function() {
+    $(".modal-canvas").toggleClass("hidden");
+ });
+});
